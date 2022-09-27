70e28a8554722e7d391311948b209045b540d3b3
FaiberHdz <93232600+FaiberHernandez@users.noreply.github.com>
2022-09-27 04:37:47 -0500

    Add files via upload
    Listo

--- creativeCode/app.js
+++ creativeCode/app.js
@@ -323,4 +323,42 @@ function showPopup() {
 
 //End validating form fields
 
-//Pop up 
+//Hamburger nav menu
+
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+//End hamburger menu
+
+//Scroll Reveal
+
+
+ScrollReveal().reveal("#about", { delay: 250 });
+ScrollReveal().reveal("#portafolio", { delay: 250 });
+ScrollReveal().reveal("#skills", { delay: 250 });
+ScrollReveal().reveal("#contact", { delay: 250 });
+
+//End scroll reveal
